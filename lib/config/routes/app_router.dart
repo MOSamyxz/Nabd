83e9e4e27c3b35be3da610440fd6e6af229b38c5
MOo <moosamyz9@email.com>
2025-08-11 16:20:46 +0300

    add routing service

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:nabd/features/home/presentation/pages/home_page.dart';
+import 'package:nabd/config/injection/injection.dart';
+import 'package:nabd/features/auth/presentation/log_in/login_cubit.dart/login_cubit.dart';
+import 'package:nabd/features/auth/presentation/log_in/pages/login_page.dart';
+import 'package:nabd/features/auth/presentation/sign_up/cubit/sign_up_cubit.dart';
+import 'package:nabd/features/auth/presentation/sign_up/pages/sign_up_page.dart';
 import 'app_routes.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: AppRoutes.authGate,
+  initialLocation: AppRoutes.login,
   routes: [
-    GoRoute(
+    /*   GoRoute(
       path: AppRoutes.authGate,
       builder: (context, state) => const HomePage(),
+    ), */
+    GoRoute(
+      path: AppRoutes.login,
+      builder:
+          (context, state) => BlocProvider(
+            create: (context) => sl<LoginCubit>(),
+            child: const LoginPage(),
+          ),
     ),
-  
+    GoRoute(
+      path: AppRoutes.signUp,
+      builder:
+          (context, state) => BlocProvider(
+            create: (context) => sl<SignUpCubit>(),
+            child: const SignUpPage(),
+          ),
+    ),
   ],
   errorBuilder:
       (context, state) =>
@@ -25,4 +45,3 @@ CustomTransitionPage buildFadeTransition(Widget child) {
             FadeTransition(opacity: animation, child: child),
   );
 }
- 
