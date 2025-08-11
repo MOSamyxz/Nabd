384e3fa8c375335d44a51e80f40f87f973b1f72b
MOo <moosamyz9@email.com>
2025-08-11 22:08:33 +0300

    update on main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive/hive.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:nabd/core/cubits/locale/locale_cubit.dart';
 import 'package:nabd/core/cubits/theme/theme_cubit.dart';
 import 'package:nabd/core/cubits/network/network_cubit.dart';
 import 'package:nabd/features/auth/data/models/user_model.dart';
@@ -27,15 +30,21 @@ Future<void> main() async {
   );
   final appDocumentDir = await getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
+  Hive.registerAdapter(UserModelAdapter());
   await Hive.openBox<UserModel>('userBox');
 
   await initServiceLocator();
-
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => sl<NetworkCubit>()),
         BlocProvider(create: (_) => ThemeCubit()),
+        BlocProvider(create: (_) => LocaleCubit()),
       ],
       child: NabdApp(),
     ),
